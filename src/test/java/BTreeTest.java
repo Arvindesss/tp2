fa5caa9fea124a3a5cbb6a130a9d5520747c06c9
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2024-02-28 12:27:17 +0100

    add comments

--- src/test/java/BTreeTest.java
+++ src/test/java/BTreeTest.java
@@ -30,7 +30,7 @@ public class BTreeTest {
 
     @Test
     public void testSearchWithOddAndEvenNumbers(){
-        // Given a BTree with a sequence of numbers 1,2,3,4,5 and m = 3
+        // Given a BTree with a sequence of numbers with odd then even numbers up to 99 and m = 3
         BTree btree = new BTree(3,1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98);
         // When we search these numbers in the BTree
         for (int i = 1; i <= 99; i++) {
@@ -41,7 +41,7 @@ public class BTreeTest {
 
     @Test
     public void testSearchWith1to20(){
-        // Given a BTree with a sequence of numbers 1,2,3,4,5 and m = 3
+        // Given a BTree with a sequence of numbers 1 to 20 and m = 4
         BTree btree = new BTree(4,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
         // When we search these numbers in the BTree
         for (int i = 1; i <= 20; i++) {
@@ -52,32 +52,17 @@ public class BTreeTest {
 
 
     @Test
-    public void testSearchWithEvenTreeOrder(){
-        // Given a BTree with a sequence of numbers 1,2,3,4,5 and m = 3
-        BTree btree = new BTree(4,46,1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 2, 4, 6, 8, 10, 12, 14, 16);
-        // When we search these numbers in the BTree
+    public void testSearchWithOddTreeOrder(){
+        // Given a BTree with 46 first and then with a sequence of odd numbers up to 99 and m = 4
+        BTree btree = new BTree(4,46,1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99);
+        // When we search these 46 in the BTree
+        //Then we should find it
         assertTrue(btree.searchKey(46));
-    }
-
-
-    @Test
-    public void testSearchWithGreatNumberFirst(){
-        // Given a BTree with a sequence of numbers 1,2,3,4,5 and m = 3
-        BTree btree = new BTree(3,70,72, 74,76, 78, 1,2,3);
-        // When we search these numbers in the BTree
-        assertTrue(btree.searchKey(70));
-        assertTrue(btree.searchKey(72));
-        assertTrue(btree.searchKey(74));
-        assertTrue(btree.searchKey(76));
-        assertTrue(btree.searchKey(78));
-
-        for (int i = 1; i <= 3; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 99; i = i + 2) {
             //Then we expect to find all keys
             assertTrue(btree.searchKey(i));
         }
     }
-
 
     @Test
     public void testWith12345(){
