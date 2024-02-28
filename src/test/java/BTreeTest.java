a57b1126cf287d2ff36f73b268ff134cae2b7ee1
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2024-02-28 14:25:00 +0100

    add comments

--- src/test/java/BTreeTest.java
+++ src/test/java/BTreeTest.java
@@ -40,7 +40,7 @@ public class BTreeTest {
     }
 
     @Test
-    public void testSearchWith1to20(){
+    public void testSearchWithEvenM(){
         // Given a BTree with a sequence of numbers 1 to 20 and m = 4
         BTree btree = new BTree(4,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
         // When we search these numbers in the BTree
